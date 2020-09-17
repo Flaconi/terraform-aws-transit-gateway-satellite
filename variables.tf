2354eceecccd158e79dbe0ed2be3ff4f7841ac8e
Sergey Novikov <sergey.novikov@flaconi.de>
2020-09-17 18:56:16 +0200

    Update description for parameter

--- variables.tf
+++ variables.tf
@@ -81,7 +81,7 @@ variable "attachment_subnet_filters" {
 }
 
 variable route_private_subnets_via_tgw {
-  description = "Use TGW attachment as a default route for private subnets"
+  description = "Use TGW attachment as a default route (0.0.0.0/0) for private subnets. Value `satellite_destination_cidr_block`s will be ignored."
   type        = bool
   default     = false
 }
