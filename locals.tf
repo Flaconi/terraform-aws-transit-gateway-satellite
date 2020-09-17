094b78b9619aff4531c7e42a2bf9627bc872697d
Sergey Novikov <sergey.novikov@flaconi.de>
2020-09-17 18:44:04 +0200

    Use 0.0.0.0/0 for private subnets if flag is set

--- locals.tf
+++ locals.tf
@@ -5,10 +5,26 @@ locals {
 
   transit_gateway_route_table_id = var.transit_gateway_route_table_id == "" ? data.aws_ec2_transit_gateway_route_table.this[0].id : var.transit_gateway_route_table_id
 
-  routes_in_tables = [
-    for pair in setproduct(var.route_entire_satellite_vpc ? data.aws_route_tables.all[0].ids : data.aws_route_table.this[*].route_table_id, var.satellite_destination_cidr_blocks) : {
+  routing_tables_all     = data.aws_route_tables.all[0].ids
+  routing_tables_private = data.aws_route_table.this[*].route_table_id
+  routing_tables_rest    = setsubtract(local.routing_tables_all, local.routing_tables_private)
+
+  routes_in_tables_private = [
+    for pair in setproduct(local.routing_tables_private, var.route_private_subnets_via_tgw ? ["0.0.0.0/0"] : var.satellite_destination_cidr_blocks) : {
       table_id        = pair[0]
       dest_cidr_block = pair[1]
     }
   ]
+
+  routes_in_tables_rest = [
+    for pair in setproduct(local.routing_tables_rest, var.satellite_destination_cidr_blocks) : {
+      table_id        = pair[0]
+      dest_cidr_block = pair[1]
+    }
+  ]
+
+  routes_in_tables_all = concat(local.routes_in_tables_private, local.routes_in_tables_rest)
+
+  routes_in_tables = var.route_entire_satellite_vpc ? local.routes_in_tables_all : local.routes_in_tables_private
+
 }
