0ced69308581e120e57c949460d53d5f157d1745
Sergey Novikov <sergey.novikov@flaconi.de>
2021-09-28 12:53:12 +0200

    Remove obsolete list function

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "transit_gateway_vpc_attachment_id" {
   description = "Identifier of the Transit Gateway VPC Attachment"
-  value       = element(concat(aws_ec2_transit_gateway_vpc_attachment.this.*.id, list("")), 0)
+  value       = element(concat(aws_ec2_transit_gateway_vpc_attachment.this.*.id, []), 0)
 }
